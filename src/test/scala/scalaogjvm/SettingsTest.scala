f66c5f9e5ca1427b452d5b8b152d637ea4404990
Frode Nerbråten <frode@nerbraten.no>
2013-09-16 15:33:12 +0200

    Klar til oppgaveløsing

--- src/test/scala/scalaogjvm/SettingsTest.scala
+++ src/test/scala/scalaogjvm/SettingsTest.scala
@@ -6,14 +6,14 @@ import com.typesafe.config.{ConfigException, ConfigFactory}
 class SettingsTest extends FlatSpec {
   "The settings object" should "load configuration for 'my-app' from application.conf" in {
     val settings = Settings.load()
-    assert(settings.env === "dev")
-    assert(settings.jdbcUrl === "jdb:hsql:mem")
-    assert(settings.fileSize === 1000000)
+//    assert(settings.env === "dev")
+//    assert(settings.jdbcUrl === "jdb:hsql:mem")
+//    assert(settings.fileSize === 1000000)
   }
 
   it should "load secret properties from secret.conf" in {
     val settings = Settings.load()
-    assert(settings.jdbcPassword === "secret-password")
+//    assert(settings.jdbcPassword === "secret-password")
   }
 
   it should "fail fast on missing config keys" in {
@@ -24,8 +24,8 @@ class SettingsTest extends FlatSpec {
 
   it should "override default values from environment specific sections" in {
     val settings = Settings.load("qa")
-    assert(settings.env === "qa")
-    assert(settings.jdbcUrl === "jdbc:enterprise:oracle")
+//    assert(settings.env === "qa")
+//    assert(settings.jdbcUrl === "jdbc:enterprise:oracle")
   }
 
   it should "load user specific username and password from system properties" in {
@@ -33,8 +33,8 @@ class SettingsTest extends FlatSpec {
     System.setProperty("my-app.jdbc.password", "db-123")
     ConfigFactory.invalidateCaches()
     val settings = Settings.load()
-    assert(settings.jdbcUser === "frode")
-    assert(settings.jdbcPassword === "db-123")
+//    assert(settings.jdbcUser === "frode")
+//    assert(settings.jdbcPassword === "db-123")
     System.clearProperty("my-app.jdbc.username")
     System.clearProperty("my-app.jdbc.password")
     ConfigFactory.invalidateCaches()
@@ -42,13 +42,13 @@ class SettingsTest extends FlatSpec {
 
   it should "load time units as milliseconds" in {
     val settings = Settings.load()
-    assert(settings.jdbcTimeout === 30000)
+//    assert(settings.jdbcTimeout === 30000)
   }
 
-  it should "override settings using profiles" in {
+  it should "override settings using arbitrary profiles" in {
     val settings = Settings.load("qa", "debugging", "slow-db")
-    assert(settings.env === "qa")
-    assert(settings.debug === true)
-    assert(settings.jdbcTimeout === 5*60000)
+//    assert(settings.env === "qa")
+//    assert(settings.debug === true)
+//    assert(settings.jdbcTimeout === 5*60000)
   }
 }
